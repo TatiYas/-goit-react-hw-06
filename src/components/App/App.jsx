03239b77a5b0625d58cb59dcb41cac0a5980c1f4
TatiYas <iasinovska_lawyer@ukr.net>
2024-08-12 13:29:37 +0200

    Update_1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,15 +2,34 @@ import s from "./App.module.css";
 import ContactForm from "../ContactForm/ContactForm";
 import SearchBox from "../SearchBox/SearchBox";
 import ContactList from "../ContactList/ContactList";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { selectError, selectLoading } from "../../redux/contactsSlice";
+import { fetchContacts } from "../../redux/contactsOps";
+
+
+
 
 function App() {
-  return (
-    <div className={s.container}>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
-    </div>
-  );
+ const dispatch = useDispatch();
+ const loading = useSelector(selectLoading);
+ const error = useSelector(selectError);
+
+
+ useEffect(() => {
+   dispatch(fetchContacts());
+ }, [dispatch]);
+
+
+ return (
+   <div className={s.container}>
+     <h1>Phonebook</h1>
+     <ContactForm />
+     <SearchBox />
+     {error && <b>Ooops ... something went wrong!</b>}
+     {loading && <Loader/>}
+     <ContactList />
+   </div>
+ );
 }
 export default App;
