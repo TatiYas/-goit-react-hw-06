77be0af4d089c0983237946efa1fcf481e7e17bd
TatiYas <iasinovska_lawyer@ukr.net>
2024-08-12 13:47:37 +0200

    Update_4

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
 
-
 export const store = configureStore({
-   reducer: {
-       contacts: contactsReducer,
-       filters: filtersReducer,
-   },
+    reducer: {
+        contacts: contactsReducer,
+        filters: filtersReducer,
+    },
 });
