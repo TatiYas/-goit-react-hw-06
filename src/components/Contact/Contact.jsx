7fd40980e486a203c3ff31c509def8e5d0fd5d49
TatiYas <iasinovska_lawyer@ukr.net>
2024-08-12 13:38:54 +0200

    Update_2

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,32 +1,35 @@
 import { useDispatch } from "react-redux";
-import { deleteContact } from "../../redux/contactsSlice";
 import s from "./Contact.module.css";
+import { deleteContact } from "../../redux/contactsOps";
 import { BsFillTelephoneFill, BsFillPersonFill } from "react-icons/bs";
 
-export const Contact = ({ item: { id, name, number } }) => {
-  const dispatch = useDispatch();
-  const handleDelete = () => dispatch(deleteContact(id));
 
-  return (
-    <>
-      <div>
-        <div className={s.element}>
-          <span className={s.icon}>
-            <BsFillPersonFill size={35} color="purple" />
-          </span>
-          <p className={s.text}>{name}</p>
-        </div>
-        <div className={s.element}>
-          <span className={s.icon}>
-            <BsFillTelephoneFill size={35} color="purple" />
-          </span>
-          <p className={s.text}>{number}</p>
-        </div>
-      </div>
+export const Contact = ({ contact: { id, name, number } }) => {
+ const dispatch = useDispatch();
+ const handleDelete = () => dispatch(deleteContact(id));
 
-      <button className={s.btn} onClick={handleDelete} type="submit">
-        Delete
-      </button>
-    </>
-  );
+
+ return (
+   <>
+     <div>
+       <div className={s.element}>
+         <span className={s.icon}>
+           <BsFillPersonFill size={30} color="black" />
+         </span>
+         <p className={s.text}>{name}</p>
+       </div>
+       <div className={s.element}>
+         <span className={s.icon}>
+           <BsFillTelephoneFill size={30} color="black" />
+         </span>
+         <p className={s.text}>{number}</p>
+       </div>
+     </div>
+
+
+     <button className={s.btn} onClick={handleDelete} type="submit">
+       Delete
+     </button>
+   </>
+ );
 };
