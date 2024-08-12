6e9fd71ffc882f8773df76658f64d573a0094aba
TatiYas <150725014+TatiYas@users.noreply.github.com>
2024-08-12 22:23:26 +0200

    Update SearchBox.jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -3,27 +3,25 @@ import { changeFilter, selectNameFilter } from "../../redux/filtersSlice";
 import { useId } from "react";
 import s from "./SearchBox.module.css";
 
-
 function SearchBox() {
- const searchId = useId();
- const dispatch = useDispatch();
- const value = useSelector(selectNameFilter);
+  const searchId = useId();
+  const dispatch = useDispatch();
+  const value = useSelector(selectNameFilter);
 
-
- const onFilter = (e) => dispatch(changeFilter(e.target.value));
- return (
-   <div className={s.box}>
-     <label className={s.label} htmlFor={searchId}>
-       Find contacts by name
-     </label>
-     <input
-       className={s.input}
-       id={searchId}
-       type="text"
-       value={value}
-       onChange={onFilter}
-     />
-   </div>
- );
+  const onFilter = (e) => dispatch(changeFilter(e.target.value));
+  return (
+    <div className={s.box}>
+      <label className={s.label} htmlFor={searchId}>
+        Find contacts by name
+      </label>
+      <input
+        className={s.input}
+        id={searchId}
+        type="text"
+        value={value}
+        onChange={onFilter}
+      />
+    </div>
+  );
 }
 export default SearchBox;
